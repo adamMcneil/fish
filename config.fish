a2f97b8fe93182ffdeb9b19567085fff78f3492c
adamwm4 <adamwm4@illinois.edu>
2024-09-14 14:39:04 -0500

    added lines to make other stuff not throw errors

--- config.fish
+++ config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
 	starship init fish | source	
 end
-theme_tokyonight night 
+
+# TeX
+export PATH="/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin:$PATH"
+
+# opam configuration
+source /home/adam/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
